8b06e2b40d055c4edb62c36c65f7064eaae3282f
Abraão Barbosa <abraaobarbosati@gmail.com>
2023-02-05 14:36:56 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const console = document.getElementById("comands");
+const consoleTextInput = document.getElementsByClassName("comand-active");
+
+console.addEventListener("click", () => {
+    consoleTextInput[0].focus();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+function hasClass(ele, cls) {
+    return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
